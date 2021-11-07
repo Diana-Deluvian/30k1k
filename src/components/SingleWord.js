e05edc945e9fb393b86d4861842d23dcbb737304
Diana Deluvian <diana.deluvian@gmail.com>
2021-11-07 21:46:18 +0100

    added checks for empty array on words

--- src/components/SingleWord.js
+++ src/components/SingleWord.js
@@ -1,20 +1,27 @@
 import React, { useContext } from 'react';
 import WordsContext from '../context/WordsContext';
+import _ from 'lodash';
 
 const Word = () => {
-    const { words, setWords } = useContext(WordsContext);
-    let {wordname, meaning, example, additionalInfo, type, id } = words.at(-1);
-    console.log(words);
-    //gets the last word
-    return (
-        <div className="single-word">
-      <h1>{wordname}</h1>
-      <p className="type">verb</p>
-      <p className="definition">{meaning}</p>
-      <p className="example">"{example}"</p>
-      <p className="additionalInfo">{additionalInfo}</p>
-    </div>
-    )
+  const { words, setWords } = useContext(WordsContext);
+    let {wordname, meaning, example, additionalInfo, type, id } = words.at(-1) || {};
+  //gets the last word
+  return (
+    <React.Fragment>
+      
+        {!_.isEmpty(words) ? (
+          <div className="single-word">
+            <h1>{wordname}</h1>
+            <p className="type">verb</p>
+            <p className="definition">{meaning}</p>
+            <p className="example">"{example}"</p>
+            <p className="additionalInfo">{additionalInfo}</p>
+          </div>
+          ) : (
+            <p className="message">No words available. Please add some words.</p>
+          )}
+  </React.Fragment> )
+
 }
 
 export default Word;
