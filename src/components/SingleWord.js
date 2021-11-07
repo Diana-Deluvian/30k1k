6ab635deef18ba4e68816056dd47e4cbf7669ad2
Diana Deluvian <diana.deluvian@gmail.com>
2021-11-07 23:32:21 +0100

    added random word button

--- src/components/SingleWord.js
+++ src/components/SingleWord.js
@@ -1,11 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import WordsContext from '../context/WordsContext';
 import _ from 'lodash';
+import { Button } from 'react-bootstrap';
 
 const Word = () => {
   const { words, setWords } = useContext(WordsContext);
-    let {wordname, meaning, example, additionalInfo, type, id } = words.at(-1) || {};
-  //gets the last word
+  const [word, setWord] = useState(words.at(-1) || {});
+    //gets the last word
+    let {wordname, meaning, example, additionalInfo, type, id } = word; 
+
   return (
     <React.Fragment>
       
@@ -16,10 +19,15 @@ const Word = () => {
             <p className="definition">{meaning}</p>
             <p className="example">"{example}"</p>
             <p className="additionalInfo">{additionalInfo}</p>
+            <Button variant="light" onClick={()=> { 
+            let newWord = words.at(Math.floor(Math.random() * words.length))
+            setWord(newWord) } }>Random!</Button>
           </div>
           ) : (
             <p className="message">No words available. Please add some words.</p>
           )}
+
+          
   </React.Fragment> )
 
 }
